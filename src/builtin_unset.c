b4e810596dbea63b17037134c1b1996649e69f1a
Tom Jans <tjans@fluffderg.me>
2021-03-05 19:11:46 +0100

    Unset can remove multiple environment variables at the same time

--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -5,12 +5,19 @@
 
 int	builtin_unset(int argc, char **argv, t_state *state)
 {
-	if (argc != 2)
+	int i;
+
+	i = 1;
+	if (argc < 2)
 	{
 		printf("%s: not enough arguments\n", argv[0]);
 		return (-1);
 	}
-	bucket_del(state->env->env, argv[1]);
+	while (i < argc)
+	{
+		bucket_del(state->env->env, argv[i - 1]);
+		i++;
+	}
 	env_update_envp(state->env);
 	return (0);
 }
