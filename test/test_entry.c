4f8e3db78f380dc2d535ea19bb0c73841b6e8e04
Tom <tnjans@outlook.de>
2021-03-31 18:38:04 +0200

    Add hashtable test

--- test/test_entry.c
+++ test/test_entry.c
@@ -5,6 +5,7 @@ int	main(void)
 {
 	printf("Minishell testing program.\n");
 	test_env();
+	test_hashtable();
 	printf("All tests ran.\n");
 	return (0);
 }
--- test/tests.h
+++ test/tests.h
@@ -2,6 +2,7 @@
 # define TESTS_H
 
 void	test_env(void);
+void	test_hashtable(void);
 void	uassert(int condition, char *src);
 
 #endif
