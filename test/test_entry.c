dded020ef7751b65c58a284e15c220c9dd7486b1
Tom <tnjans@outlook.de>
2021-04-05 14:51:36 +0200

    Add tokenizer test

--- test/test_entry.c
+++ test/test_entry.c
@@ -6,6 +6,7 @@ int	main(void)
 	printf("Minishell testing program.\n");
 	test_env();
 	test_hashtable();
+	test_tokenizer();
 	printf("All tests ran.\n");
 	return (0);
 }
--- test/tests.h
+++ test/tests.h
@@ -3,6 +3,7 @@
 
 void	test_env(void);
 void	test_hashtable(void);
+void	test_tokenizer(void);
 void	uassert(int condition, char *src);
 
 #endif
