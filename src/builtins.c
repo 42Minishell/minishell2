f7997f045c39a2fd7c4dbb37d45562079af3b6c6
Tom Jans <tjans@fluffderg.me>
2022-02-14 19:43:31 +0100

    refactor: lookup table is now a const static global

--- src/builtins.c
+++ src/builtins.c
@@ -14,41 +14,28 @@
 
 #include "builtins.h"
 
-static void	set_table(t_function_lookup *entry, \
-	char *key, void *func_ptr)
-{
-	entry->exec = key;
-	entry->func_ptr = func_ptr;
-}
-
-//FIXME: MAKE IT A LOOKUP TABLE GLOBAL THING PROPER BLA
-
-static t_function_lookup	*get_table(t_function_lookup *lookuptable)
-{
-	set_table(lookuptable, "unset", &builtin_unset);
-	set_table(lookuptable + 1, "export", &builtin_export);
-	set_table(lookuptable + 2, "echo", &builtin_echo);
-	set_table(lookuptable + 3, "env", &builtin_export);
-	set_table(lookuptable + 4, "pwd", &builtin_pwd);
-	set_table(lookuptable + 5, "cd", &builtin_cd);
-	set_table(lookuptable + 6, "exit", &builtin_exit);
-	set_table(lookuptable + 7, "", NULL);
-	return (lookuptable);
-}
+const static t_function_lookup	g_lookup_table[8] = {
+{"unset", &builtin_unset},
+{"export", &builtin_export},
+{"echo", &builtin_echo},
+{"env", &builtin_export},
+{"pwd", &builtin_pwd},
+{"cd", &builtin_cd},
+{"exit", &builtin_exit},
+{"", NULL},
+};
 
 void	*find_builtin_func(char *exec)
 {
-	size_t				i;
-	size_t				e_len;
-	t_function_lookup	table[8];
+	size_t	i;
+	size_t	e_len;
 
 	i = 0;
 	e_len = ft_strlen(exec);
-	get_table(table);
-	while (table[i].func_ptr)
+	while (g_lookup_table[i].func_ptr)
 	{
-		if (ft_strncmp(exec, table[i].exec, e_len + 1) == 0)
-			return (table[i].func_ptr);
+		if (ft_strncmp(exec, g_lookup_table[i].exec, e_len + 1) == 0)
+			return (g_lookup_table[i].func_ptr);
 		i++;
 	}
 	return (NULL);
