724d695009470c60024431973328917784ebb97b
Tom Jans <tjans@fluffderg.me>
2022-02-26 09:28:16 +0100

    io: the aamount of wtfs per second here was enourmous. closes #42

--- src/io.c
+++ src/io.c
@@ -14,43 +14,37 @@
 #include <unistd.h>
 #include "io.h"
 
-static int	setup_redir_to_overwrite(t_token *redir, t_token *prev)
+static int	setup_redir_to_overwrite(t_token *redir)
 {
 	int	fd;
 
 	fd = open(redir->token, O_CREAT | O_WRONLY | O_TRUNC, 0666);
 	if (fd < 0)
 		return (0);
-	prev->next = redir->next;
-	free(redir);
 	if (dup2(fd, 1) == -1)
 		return (0);
 	return (1);
 }
 
-static int	setup_redir_to_append(t_token *redir, t_token *prev)
+static int	setup_redir_to_append(t_token *redir)
 {
 	int	fd;
 
 	fd = open(redir->token, O_CREAT | O_WRONLY | O_APPEND, 0666);
 	if (fd < 0)
 		return (0);
-	prev->next = redir->next;
-	free(redir);
 	if (dup2(fd, 1) == -1)
 		return (0);
 	return (1);
 }
 
-static int	setup_redir_from_file(t_token *redir, t_token *prev)
+static int	setup_redir_from_file(t_token *redir)
 {
 	int	fd;
 
 	fd = open(redir->token, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	prev->next = redir->next;
-	free(redir);
 	if (dup2(fd, 0) == -1)
 		return (0);
 	return (1);
@@ -58,22 +52,19 @@ static int	setup_redir_from_file(t_token *redir, t_token *prev)
 
 int	io_setup(t_token *head)
 {
-	t_token			*head_prev;
 	int				res;
 
 	res = 1;
-	head_prev = head;
 	head = head->next;
 	while (head && head->type != executable && head->type != redirect_to_pipe)
 	{
 		if (head->type == redirect_to_overwrite)
-			res = setup_redir_to_overwrite(head, head_prev);
+			res = setup_redir_to_overwrite(head);
 		if (head->type == redirect_to_append)
-			res = setup_redir_to_append(head, head_prev);
+			res = setup_redir_to_append(head);
 		if (head->type == redirect_from_file || \
 			head->type == redirect_from_file_and_unlink)
-			res = setup_redir_from_file(head, head_prev);
-		head_prev = head;
+			res = setup_redir_from_file(head);
 		head = head->next;
 	}
 	return (res);
