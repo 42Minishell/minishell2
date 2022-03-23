0440e2943fd116104f2660f54a2dea892a124043
Zinobias Gargasch <zgargasc@f0r4s21.codam.nl>
2022-03-23 20:31:58 +0100

    pimped da prompt

--- src/prompt.c
+++ src/prompt.c
@@ -6,20 +6,21 @@
 /*   By: zgargasc <zgargasc@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/18 17:13:58 by zgargasc      #+#    #+#                 */
-/*   Updated: 2021/12/18 17:13:59 by zgargasc      ########   odam.nl         */
+/*   Updated: 2022/03/23 20:24:12 by zgargasc      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Created by Tom Jans on 01-03-21.
 
 #include "minishell.h"
+#include <stdio.h>
 
 char	*prompt(t_state *state)
 {
 	char	*input;
 
 	(void)state;
-	input = readline("MS > ");
+	input = readline("\033[1;36mMS\033[0m > \033[1;31m");
 	if (!input)
 	{
 		printf("\n");
